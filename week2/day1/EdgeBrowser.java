aa1541993df6c487e17f91bf62df0e5299451b6c
ManiTestLeaf1 <rxdenied@gmail.com>
2025-04-05 17:49:39 -0700

    Add files via upload

---
+++ week2/day1/EdgeBrowser.java
@@ -0,0 +1,13 @@
+package week2.day1;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+
+		Browser browserClass2=new Browser();
+		browserClass2.launchBrowser("Browser launched successfully");
+		browserClass2.loadUrl();
+
+	}
+
+}
