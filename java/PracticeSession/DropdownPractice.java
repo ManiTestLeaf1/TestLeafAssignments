6d2aac57d2b468c914eaec4c5d46bf7e575619ca
ManiTestLeaf1 <rxdenied@gmail.com>
2025-04-11 00:19:29 -0700

    Add files via upload

---
+++ java/PracticeSession/DropdownPractice.java
@@ -0,0 +1,10 @@
+package PracticeSession;
+
+public class DropdownPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/week2/day1/LibraryManagement.java
@@ -0,0 +1,12 @@
+package week2.day1;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		Library libManageObj = new Library();
+		libManageObj.addBook("Book added successfully");
+		libManageObj.issueBook();
+
+	}
+
+}
