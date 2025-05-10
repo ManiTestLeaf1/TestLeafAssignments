e8862abcec6dfbd2649d7bbc3b62859b601df92b
ManiTestLeaf1 <rxdenied@gmail.com>
2025-05-10 00:43:10 -0700

    Delete java directory

--- java/runner/RunnerClass.java
+++
@@ -1,9 +0,0 @@
-package runner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features ="src/main/java/features" ,glue="steps" ,monochrome=true, publish=true, tags="@Sanity")
-public class RunnerClass extends AbstractTestNGCucumberTests {
-
-}
